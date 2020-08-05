279f2f3a266641346645806c02f9f587f871f0ee
carlosy <yanezpena@gmail.com>
2020-08-05 00:52:52 -0400

    add notifications

--- src/app/shared/services/notification.service.ts
+++ src/app/shared/services/notification.service.ts
@@ -1,71 +1,57 @@
-import { Injectable } from "@angular/core";
-import { ToastrService } from "ngx-toastr";
+import { Injectable } from '@angular/core';
+import {
+	MatSnackBarHorizontalPosition,
+	MatSnackBarVerticalPosition,
+	MatSnackBar,
+} from '@angular/material/snack-bar';
 
 @Injectable()
 export class NotificationService {
-	constructor(private toastr: ToastrService) {}
+	horizontalPosition: MatSnackBarHorizontalPosition = 'right';
+	verticalPosition: MatSnackBarVerticalPosition = 'top';
+	duration: number = 1500;
 
-	// public messageBox(type: string, message: string, title: string) {
-	// 	let text = `<span class="tim-icons icon-bell-55" [data-notify]="icon"></span> ${message}.`;
-	// 	let classes = {
-	// 		toastClass: `alert alert-${type} alert-with-icon`,
-	// 		closeButton: true,
-	// 		enableHtml: true,
-	// 		positionClass: "toast-bottom-center"
-	// 	};
-	// 	switch (type) {
-	// 		case "success": {
-	// 			this.toastr.success(text, title, classes);
-	// 			break;
-	// 		}
-	// 		case "info": {
-	// 			this.toastr.info(text, title, classes);
-	// 			break;
-	// 		}
-	// 		case "warning": {
-	// 			this.toastr.warning(text, title, classes);
-	// 			break;
-	// 		}
-	// 		case "error": {
-	// 			this.toastr.error(text, title, classes);
-	// 			break;
-	// 		}
-	// 		default: {
-	// 			this.toastr.success(text, title, classes);
-	// 			break;
-	// 		}
-	// 	}
-	// }
+	constructor(private _snackBar: MatSnackBar) {}
 
-	span = `<span class="tim-icons icon-bell-55" [data-notify]="icon"></span>`;
-	classes = {
-		toastClass: `alert alert-info alert-with-icon`,
-		closeButton: true,
-		enableHtml: true,
-		positionClass: "toast-bottom-center"
-	};
+	openSnackBar(message: string) {
+		this._snackBar.open(message, '', {
+			duration: this.duration,
+			horizontalPosition: this.horizontalPosition,
+			verticalPosition: this.verticalPosition,
+			panelClass:['success-notification'],
+		});
+	}
+
+	// span = `<span class="tim-icons icon-bell-55" [data-notify]="icon"></span>`;
+	// classes = {
+	// 	toastClass: `alert alert-info alert-with-icon`,
+	// 	closeButton: true,
+	// 	enableHtml: true,
+	// 	positionClass: "toast-bottom-center"
+	// };
 
 	public success(message: string, title: string) {
-		let text = this.span + ` ${message}.`;
-		this.classes.toastClass = "alert alert-success alert-with-icon";
-		this.toastr.success(text, title, this.classes);
+		// let text = this.span + ` ${message}.`;
+		// this.classes.toastClass = "alert alert-success alert-with-icon";
+		this.openSnackBar(message);
 	}
 
-	public info(message: string, title: string) {
-		let text = this.span + ` ${message}.`;
-		this.classes.toastClass = "alert alert-info alert-with-icon";
-		this.toastr.info(text, title, this.classes);
-	}
+	// public info(message: string, title: string) {
+	// 	let text = this.span + ` ${message}.`;
+	// 	this.classes.toastClass = "alert alert-info alert-with-icon";
+	// 	this.toastr.info(text, title, this.classes);
+	// }
 
-	public warning(message: string, title: string) {
-		let text = this.span + ` ${message}.`;
-		this.classes.toastClass = "alert alert-warning alert-with-icon";
-		this.toastr.warning(text, title, this.classes);
-	}
+	// public warning(message: string, title: string) {
+	// 	let text = this.span + ` ${message}.`;
+	// 	this.classes.toastClass = "alert alert-warning alert-with-icon";
+	// 	this.toastr.warning(text, title, this.classes);
+	// }
 
 	public error(message: string, title: string) {
-		let text = this.span + ` ${message}.`;
-		this.classes.toastClass = "alert alert-error alert-with-icon";
-		this.toastr.error(text, title, this.classes);
+		// 	let text = this.span + ` ${message}.`;
+		// 	this.classes.toastClass = "alert alert-error alert-with-icon";
+		// 	this.toastr.error(text, title, this.classes);
+		this.openSnackBar(message);
 	}
 }
